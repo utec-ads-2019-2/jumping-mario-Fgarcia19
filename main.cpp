c48d192ff4237f4737ea1485df3545c370dbb35e
Fgarcia19 <fabrizio.garcia@utec.edu.pe>
2019-08-22 19:16:04 +0000

    done

--- main.cpp
+++ main.cpp
@@ -1,8 +1,62 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-int main() {
-	return EXIT_SUCCESS;
+void jumps(vector<int>walls){
+    int high=0,low=0;
+    for(int i=0;i<walls.size()-1;i++){
+        if(walls[i]<walls[i+1]){
+            high++;
+        }
+        else if(walls[i]>walls[i+1]){
+            low++;
+        }
+    }
+    cout<<high<<" "<<low<<endl;
+
 }
 
+
+vector<int> test(int numer_walls){
+    vector<int> walls;
+    int height;
+    for(int i=0;i<numer_walls;i++){
+        cin>>height;
+        walls.push_back(height);
+    }
+    return walls;
+//    jumps(walls);
+}
+
+//void simular(int number_test){
+//    int walls;
+//    for(int i=0;i<number_test;i++){
+//        cin>>walls;
+//        test(walls);
+//    }
+//}
+
+
+void simular(int number_test){
+    vector<vector<int>>casos;
+    int walls;
+    for(int i=0;i<number_test;i++){
+        cin>>walls;
+        casos.push_back(test(walls));
+    }
+    for(int i=0;i<casos.size();i++){
+        cout<<"Case "<<i+1<<": ";
+        jumps(casos[i]);
+    }
+}
+
+
+
+int main() {
+    int number_test;
+    cin>>number_test;
+    simular(number_test);
+
+    return 0;
+}
